453ab945b7dca416564969fc34c5915830d43e8c
Paula Leite <paula.t.leite@icloud.com>
2020-04-06 15:32:32 -0300

    changes to interhandler

--- IntentExampleTest/IntentHandler.swift
+++ IntentExampleTest/IntentHandler.swift
@@ -14,7 +14,7 @@ class IntentHandler: INExtension {
         // This is the default implementation.  If you want different objects to handle different intents,
         // you can override this and return the handler you want for that particular intent.
         
-        return self
+        return SomaHandler()
     }
     
 }
