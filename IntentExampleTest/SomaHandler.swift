138e056163d2fd4598613dca612ec43ed0c99831
Paula Leite <paula.t.leite@icloud.com>
2020-04-06 15:13:11 -0300

    Intent working

--- IntentExampleTest/SomaHandler.swift
+++ IntentExampleTest/SomaHandler.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import Intents
+
+public class SomaHandler: NSObject, SomaIntentHandling {
+    //O usuário já usou o intent e o aplicativo tem que resolver o que fazer
+    public func handle(intent: SomaIntent, completion: @escaping (SomaIntentResponse) -> Void) {
+        guard let x = intent.x, let y = intent.y else {
+            let erro = SomaIntentResponse(code: .dadosInsuficientes, userActivity: .none)
+            completion(erro)
+            return
+        }
+        let xDouble = Double(truncating: x)
+        let yDouble = Double(truncating: y)
+        let soma = NSNumber(floatLiteral: xDouble + yDouble)
+
+        let sucesso = SomaIntentResponse.success(resultado: soma)
+        completion(sucesso)
+    }
+
+    //Validar os inputs do usuário
+    public func resolveX(for intent: SomaIntent, with completion: @escaping (SomaXResolutionResult) -> Void) {
+        if let x = intent.x {
+            let xDouble = Double(truncating: x)
+            completion(SomaXResolutionResult.success(with: xDouble))
+        } else {
+            completion(SomaXResolutionResult.needsValue())
+        }
+    }
+    public func resolveY(for intent: SomaIntent, with completion: @escaping (SomaYResolutionResult) -> Void) {
+        if let y = intent.y {
+            let yDouble = Double(truncating: y)
+            completion(SomaYResolutionResult.success(with: yDouble))
+        } else {
+            completion(SomaYResolutionResult.needsValue())
+        }
+    }
+}
